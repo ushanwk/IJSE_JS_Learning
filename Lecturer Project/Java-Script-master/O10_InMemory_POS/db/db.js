b14b005e5744a460836bf1a34b989bcaac556875
UshanKaushalya <Ushanwk22@gmail.com>
2023-11-16 11:21:02 +0530

    first commit

---
+++ Lecturer Project/Java-Script-master/O10_InMemory_POS/db/db.js
@@ -0,0 +1,12 @@
+var customerDB = [
+    {id: "C00-001", name: "Sachin Thamalsha", address: "Mathara", salary: 100000},
+    {id: "C00-002", name: "Ranjith Perera", address: "Panadura", salary: 200000},
+    {id: "C00-003", name: "Kavindu Perera", address: "Panadura", salary: 300000}
+];
+
+var itemDB = [
+    {code:"I00-001",description:"Lux",qtyOnHand: 100,unitPrice: 145.00},
+    {code:"I00-002",description:"Sunlight",qtyOnHand: 150,unitPrice: 345.00},
+    {code:"I00-003",description:"Light Boy",qtyOnHand: 400,unitPrice: 245.00}
+];
+var orderDB = [];
---
+++ Lecturer Project/Java-Script-master/O1_Basics/assests/js/myScript.js
@@ -0,0 +1,3 @@
+// We can write down a script externally using a .js file and then we can
+//import it to the html page
+alert("Hello I am an External Resource..!");
